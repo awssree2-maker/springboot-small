65ba83d0ea83d1e0640268c45f822a11fa8a327d
HOME <awssree2@gmail.com>
2025-08-25 16:34:49 +0530

    ecs

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -80,16 +80,3 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   name = var.cluster_name
 }
 
-resource "aws_ecs_service" "service" {
-  name            = var.cluster_service_name
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.task_definition.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [var.vpc_id_subnet_list[0], var.vpc_id_subnet_list[1], var.vpc_id_subnet_list[2], var.vpc_id_subnet_list[3]]
-    security_groups  = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
-  }
-}
