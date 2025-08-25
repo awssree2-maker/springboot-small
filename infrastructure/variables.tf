c6ce2be22a32118a5d918b78f3898ba8b8d5bd95
HOME <awssree2@gmail.com>
2025-08-25 19:36:28 +0530

    ecs

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -42,8 +42,7 @@ variable "vpc_id_subnet_list" {
   type        = list(string)
 }
 
-# IAM execution role ARN for ECS
-# variable "execution_role_arn" {
+# IAM execution role ARN for ECSvariable "execution_role_arn" {
 #   description = "ARN of the ECS execution role used to pull images and send logs"
 #   type        = string
 # }
