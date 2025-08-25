eced1aa40b162088e58f8cd758ddbfd8860e6e83
HOME <awssree2@gmail.com>
2025-08-25 12:40:42 +0530

    updated backend infra

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,6 +1,6 @@
-# resource "aws_ecr_repository" "app" {
-#   name                 = var.name_prefix
-#   image_tag_mutability = "MUTABLE"
-#   image_scanning_configuration { scan_on_push = true }
-#   tags = { Name = "${var.name_prefix}-ecr" }
-# }
+resource "aws_ecr_repository" "app" {
+  name                 = var.name_prefix
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration { scan_on_push = true }
+  tags = { Name = "${var.name_prefix}-ecr" }
+}
