80adfaa361f160e56a812c555df9dd85d82235f2
HOME <awssree2@gmail.com>
2025-08-25 15:38:14 +0530

    updated backend infra

---
+++ infrastructure/terraform.tfvars.tf
@@ -0,0 +1,8 @@
+vpc_id                  = "vpc-0dc4dd8c5ebf8caec"
+cluster_name            = "my-ecs-cluster"
+cluster_service_name    = "springboot-service"
+cluster_service_task_name = "ecspyhtonflask"
+image_id                = "734842485697.dkr.ecr.ap-south-1.amazonaws.com/springboot-sample:latest"
+vpc_id_subnet_list      = ["subnet-0799a00e868132577", "subnet-004ed0d2e0740b093"]
+execution_role_arn      = "arn:aws:iam::734842485697:role/ecsTaskExecutionRole"
+
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,19 +6,47 @@ variable "aws_region" {
 
 
 
-variable "vpc_id" {}
+# VPC where ECS will run
+variable "vpc_id" {
+  description = "The ID of the VPC where ECS resources will be deployed"
+  type        = string
+}
 
-variable "cluster_name" {}
+# ECS Cluster name
+variable "cluster_name" {
+  description = "Name of the ECS Cluster"
+  type        = string
+}
 
-variable "cluster_service_name" {}
+# ECS Service name
+variable "cluster_service_name" {
+  description = "Name of the ECS Service that runs tasks"
+  type        = string
+}
 
-variable "cluster_service_task_name" {}
+# ECS Task Definition family name
+variable "cluster_service_task_name" {
+  description = "Name of the ECS Task Definition"
+  type        = string
+}
 
-variable "image_id" {}
+# ECR image for your container
+variable "image_id" {
+  description = "ECR image URI for the container (e.g., <account>.dkr.ecr.<region>.amazonaws.com/repo:tag)"
+  type        = string
+}
 
-variable "vpc_id_subnet_list" {}
+# Subnets in the VPC for ECS tasks
+variable "vpc_id_subnet_list" {
+  description = "List of subnet IDs in the VPC where ECS tasks should run"
+  type        = list(string)
+}
 
-variable "execution_role_arn" {}
+# IAM execution role ARN for ECS
+variable "execution_role_arn" {
+  description = "ARN of the ECS execution role used to pull images and send logs"
+  type        = string
+}
 
 
 variable "dynamodb_table_name" {
