3c90b161df0c71933b847551df82e078fca88bb5
HOME <awssree2@gmail.com>
2025-08-25 12:22:18 +0530

    updated backend infra

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "your-unique-s3-bucket-name"
+    bucket         = "my-tfstate-ap-south-1"
     key            = "global/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
-
