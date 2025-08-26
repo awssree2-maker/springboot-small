0f6efd429dd4b1e4f87f20b3440e945a7b51689d
HOME <awssree2@gmail.com>
2025-08-26 17:45:55 +0530

    a

--- infrastructure/local.tf
+++ infrastructure/local.tf
@@ -3,7 +3,7 @@ locals {
   my_vpc                 = "spring-vpc"
   my_subnet_cidr         = ["10.0.1.0/24", "10.0.2.0/24"]
   my_subnet_cidr_private = ["10.0.3.0/24", "10.0.4.0/24"]
-  bucket_name            = "spring-s3-bucket"
-  table_name             = "spring-sfsef-tfstate"
+  bucket_name            = "spring-sfserdff-bucket"
+  table_name             = "spring-dev-tfstate"
   aws_region             = "ap-south-1"
 }
