abf46a61f912bd31b50a894ca6f68ddb804cf9b3
HOME <awssree2@gmail.com>
2025-08-26 18:00:47 +0530

    a

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -11,12 +11,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "spring-s3-bucket"   # same as locals
-#     key            = "spring/terraform.tfstate"
-#     region         = "ap-south-1"
-#     dynamodb_table = "spring-dyanodb"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "spring-sfserdff-bucket" # same as locals
+    key            = "spring/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "spring-dev-tfstate"
+    encrypt        = true
+  }
+}
