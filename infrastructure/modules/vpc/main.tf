1ed3cf873343fa6eb0e4dd18b931e1905e3189c0
HOME <awssree2@gmail.com>
2025-08-26 16:01:32 +0530

    adding vpc

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block       = var.my_vpc
-  instance_tenancy = "default"
+  cidr_block           = var.my_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name = var.my_vpc
