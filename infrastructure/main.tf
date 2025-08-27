aa4c8a312c0afe1e95919397f9ecb912c4c9c811
HOME <awssree2@gmail.com>
2025-08-27 12:02:26 +0530

    sg

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 module "myvpc" {
-  source                 = "./modules/vpc"
+  source                 = "module/vpc"
   my_vpc                 = local.my_vpc
   my_cidr                = local.my_cidr
   my_subnet_cidr         = local.my_subnet_cidr
@@ -7,15 +7,15 @@ module "myvpc" {
   my_igw                 = local.my_igw
 }
 module "s3" {
-  source      = "./modules/s3"
+  source      = "module/s3"
   bucket_name = local.bucket_name
 }
 module "dyanomdb" {
-  source     = "./modules/dyanmodb"
+  source     = "module/dyanmodb"
   table_name = local.table_name
 }
 module "sg" {
-  source        = "./modules/sg"
+  source        = "module/sg"
   name          = "application-newsg"
   ingress_ports = local.ingress_ports
   allowed_cidr  = local.allowed_cidr
---
+++ infrastructure/module/vpc/outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+  value       = aws_vpc.my_vpc.id                                     # The actual value to be outputted
+  description = "The public IP address of the EC2 instance" # Description of what this output represents
+}
