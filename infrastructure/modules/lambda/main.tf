ac5c2a4ed76c25deb15c60ea3c2b4779eca92900
HOME <awssree2@gmail.com>
2025-08-30 19:34:31 +0530

    lambda

--- infrastructure/modules/lambda/main.tf
+++ infrastructure/modules/lambda/main.tf
@@ -17,9 +17,7 @@ resource "aws_lambda_function" "my_csv_to_parquet" {
 
   timeout = 60
   memory_size = 512
-  layers = [
-    "arn:aws:lambda:ap-south-1:734842485697:layer:AWSSDKPandas-Python39:25"
-  ]
+
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
